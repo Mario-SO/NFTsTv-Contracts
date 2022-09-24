86ef7fd6abf73f699a27c442985ba0d413315f02
Mario-SO <gh_mario.proclaim@slmail.me>
2022-09-24 19:27:28 +0200

    Factory contract

---
+++ src/LiveNftFactory.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./LiveNFT.sol";
+import "./CloneFactory.sol";
+
+contract LiveNftFactory is CloneFactory {
+    LiveNFT[] public livenfts;
+    address masterContract;
+
+    constructor(address _masterContract) {
+        masterContract = _masterContract;
+    }
+
+    function createLiveNFT() public {
+        address liveNFT = createClone(masterContract);
+        livenfts.push(liveNFT);
+    }
+}
