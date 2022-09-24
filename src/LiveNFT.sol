259c3a3142678245c4f1d53dc6fcaa8ee9814a81
Mario-SO <gh_mario.proclaim@slmail.me>
2022-09-24 18:52:25 +0200

    First commit

---
+++ src/LiveNFT.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "solmate/tokens/ERC721.sol";
+import "openzeppelin-contracts/utils/Strings.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
+
+error MintPriceNotPaid();
+error MaxSupply();
+error NonExistentTokenURI();
+error WithdrawTransfer();
+
+contract NFT is ERC721, Ownable {
+    using Strings for uint256;
+    string public baseTokenURI;
+    uint256 public currentTokenId;
+    uint256 public constant TOTAL_SUPPLY = 10_000;
+    uint256 public constant MINT_PRICE = 0.08 ether;
+
+    constructor(string memory _baseTokenURI) ERC721("LiveNFT", "LNFT") {
+        baseTokenURI = _baseTokenURI;
+    }
+
+    function mintTo(address recipient) public payable returns (uint256) {
+        if (msg.value != MINT_PRICE) {
+            revert MintPriceNotPaid();
+        }
+        uint256 newTokenId = ++currentTokenId;
+        if (newTokenId > TOTAL_SUPPLY) {
+            revert MaxSupply();
+        }
+        _safeMint(recipient, newTokenId);
+        return newTokenId;
+    }
+
+    function setTokenURI(string memory _baseTokenURI) public onlyOwner {
+        baseTokenURI = _baseTokenURI;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        if (ownerOf(tokenId) == address(0)) {
+            revert NonExistentTokenURI();
+        }
+        return
+            bytes(baseTokenURI).length > 0
+                ? string(abi.encodePacked(baseTokenURI, tokenId.toString()))
+                : "";
+    }
+
+    function withdrawPayments(address payable payee) external onlyOwner {
+        uint256 balance = address(this).balance;
+        (bool transferTx, ) = payee.call{value: balance}("");
+        if (!transferTx) {
+            revert WithdrawTransfer();
+        }
+    }
+}
