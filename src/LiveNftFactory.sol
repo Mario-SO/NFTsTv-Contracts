f5c58ae42e7f1deb52cd169d5f20d3a7ed8a2199
Mario-SO <gh_mario.proclaim@slmail.me>
2022-09-28 21:34:53 +0200

    TODO: Make a getter function for ContentCreators contracts

--- src/LiveNftFactory.sol
+++ src/LiveNftFactory.sol
@@ -5,9 +5,10 @@ import "./LiveNFT.sol";
 import "./CloneFactory.sol";
 
 contract LiveNftFactory is CloneFactory {
-    LiveNFT[] public livenfts;
-    mapping (address => address[]) contentCreatorsChannels;
     address masterContract;
+
+    LiveNFT[] public livenfts;
+    mapping(address => address[]) contentCreatorsChannels;
 
     constructor(address _masterContract) {
         masterContract = _masterContract;
@@ -15,7 +16,13 @@ contract LiveNftFactory is CloneFactory {
 
     function createLiveNFT() public {
         LiveNFT liveNFT = LiveNFT(createClone(masterContract));
-        liveNFT.init("https://example.com", "MyLNFT", "Welcome to my LNFT", 10, 1);
+        liveNFT.init(
+            "https://example.com",
+            "MyLNFT",
+            "Welcome to my LNFT",
+            10,
+            1
+        );
         livenfts.push(liveNFT);
         contentCreatorsChannels[msg.sender].push(address(liveNFT));
     }
